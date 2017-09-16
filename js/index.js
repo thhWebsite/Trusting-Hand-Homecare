43f7b362f55183acc9bcc45aa1c25782ac70ee5b
Ivanr553 <ivanr553@gmail.com>
2017-09-16 13:19:13 -0700

    working on logo and adding more elements

--- js/index.js
+++ js/index.js
@@ -1,12 +1,48 @@
-const state = {
-  bannerWidth: window.getComputedStyle(document.getElementById("banner")).getPropertyValue('width')
+var initState = {
+  contactBarTop: $('#contact-bar').offset().top
 }
 
+//Scroll based animatinos for page
 $(window).scroll( function() {
+
+  //Based on scroll, will ttranslate the content div up to scroll it over the banner
   $('#content').css({
     'transform' : 'translatey(' + $(window).scrollTop()*-0.25 + 'px)'
   })
-  $('#banner').css({
-    'transform' : 'scale(' + (1.1 - (0.25*($(window).scrollTop() / $(window).height()))) + ', ' + (1.1 - (0.25*($(window).scrollTop() / $(window).height()))) + ')'
+  $('#contact-bar').css({
+    'transform' : 'translatey(' + $(window).scrollTop()*-0.25 + 'px)'
   })
+
+  //Based on scroll, will blur and scale the image
+  $('#banner').css({
+    'transform' : 'scale(' + (1.1 - (0.25*($(window).scrollTop() / $(window).height()))) + ', ' + (1.1 - (0.25*($(window).scrollTop() / $(window).height()))) + ')',
+    'filter' : 'blur(' + (15*($(window).scrollTop() / $(window).height())) + 'px)'
+  })
+
+  //Cause contact bar to stickto top of page after scrolling past
+  if((initState.contactBarTop * 0.79) <= ($(window).scrollTop())) {
+    $('#contact-bar').css({
+      'position' : 'fixed',
+      'top' : '0',
+      'margin-top' : '0'
+    })
+    $('#content').css({
+      'margin-top' : '60vh'
+    })
+    $('#contact-bar').css({
+      'transform' : ''
+    })
+  }
+  if((initState.contactBarTop * 0.79) >= $(window).scrollTop()) {
+    $('#contact-bar').css({
+      'position' : '',
+      'margin-top' : '50vh'
+    })
+    $('#content').css({
+      'margin-top' : '0'
+    })
+    $('#contact-bar').css({
+      'transform' : 'translatey(' + $(window).scrollTop()*-0.25 + 'px)'
+    })
+  }
 })
